6682ce0187b499a3df94a448fcb5d22bf4cdad3f
andresnew00 <andresnew00@hotmail.com>
2018-09-13 23:34:33 -0400

    fixed pictures issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,62 @@
- // var carNumber = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// for (var i = 0; i < cardNumber.length; i++);  
 
 $(document).ready(function () {
+
+    var points = 0;
+    var randomNumber = 0;
+    var gameOver = true;
+    var wins = 0;
+    var losses = 0;
+
     function gameStarts() {
         var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-        $(".goal").text(String(randomNumber));
+        $(".goal").html(randomNumber); //try .text didnt work...
 
         var cardNumber1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
         var cardNumber2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
         var cardNumber3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
         var cardNumber4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
-        $(".score").text(String(0));
+        $(".score").text(String(0)); //found "string" online and seem to work for them but not here (what is it?)
         points = 0;
         gameOver = false;
-    }
+    });
 
-    //random number for "goal class"
-    // $(".goal").on("click", function() {
+    if (gameOver = true) {
+        gameStarts();
+    });
 
-    // });
-    //make cards click functions with random values
+    function scoreCheck() {
+        if (points > randomNumber) {
+            losses++;
+            alert("TOO BAD!");
+            gameOver = true;
+        }
+        else if (points < randomNumber) {
+            wins++;
+            alert("GOOD JOB!");
+            gameOver = true;
+        });
 
     $("#one").on("click", function () {
-
-        console.log(cardNumber1);
+        points = points + cardNumber1;
+        $(".score").text(points);
+        scoreCheck();
     });
     $("#two").on("click", function () {
-
-        console.log(cardNumber2);
+            points = points + cardNumber2;
+            $(".score").text(points);
+            scoreCheck();
     });
     $("#three").on("click", function () {
-
-        console.log(cardNumber3);
-    }); $("#four").on("click", function () {
-
-        console.log(cardNumber4);
+                points = points + cardNumber3;
+                $(".score").text(points);
+                scoreCheck();
+            });
+    $("#four").on("click", function () {
+                    points = points + cardNumber4;
+                    $(".score").text(points);
+                    scoreCheck();
     });
-    // make function that grabs clicks values and adds them together in score
 
-    //    if (scoreValue === randomNumber){
-    //        win++;
-    //        //reset game
-    //    }
-    //    else if (scoreValue > randomNumber){
-    //        losses++;
-    //        //reset game
-    //    }
 
 });
